1ae1dc5eb508d10fbae1eed6c0a3fa30a39b0409
Morgan <morganlote@gmail.com>
2021-04-15 14:27:04 +0100

    Update :-) (my git broke)

--- terrainmaker.lua
+++ terrainmaker.lua
@@ -1,64 +1,91 @@
 -- A simple terrain maker
 -- Mwgie#8873
 
+local terrainnumber = 0
 
 math.randomseed(os.time())
 
 local lastState = ''
 local CurrentState = ''
 local terrain = ''
-local hillUp = [[/
-]]
-local hillDown = [[
-\\
-]]
-for i = 1, 100 do
+local hillUp = [[ 
+/]]
+local hillDown = '\\'
 
-local function printState()
-terrain = terrain .. CurrentState
-lastState = CurrentState
-CurrentState = ''
+local function AddToTerrain()
+    terrain = terrain .. CurrentState
+    lastState = CurrentState
+    CurrentState = ''
 end
 
-local function RandomState()
+local function GenerateNextPeiceOfTerrain()
     if lastState == '_' then 
-    local number = math.random(1,4)
-    if number == 1 then CurrentState = '_'
-        elseif number == 2 then CurrentState = hillUp
-             elseif number == 3 then CurrentState = '|'
-                 elseif number == 4 then CurrentState = '~'
-                    end
-                    end
+        local number = math.random(1,4)
+        if number == 1 then 
+            CurrentState = '_'
+        elseif number == 2 then 
+            CurrentState = hillUp
+        elseif number == 3 then 
+            CurrentState = '|'
+        elseif number == 4 then 
+            CurrentState = '~'
+        end
+    end
                     
-if lastState == '/' then
-    local number = math.random(1,4)
-    if number == 1 then CurrentState = hillUp
-        elseif number == 2 then CurrentState = hillUp
-            elseif number == 3 then CurrentState = hillDown
-            else CurrentState = '_'
-            end end
-
-
-
-
-printState()
+    if lastState == '/' then
+        local number = math.random(1,4)
+        if number == 1 then 
+            CurrentState = hillUp
+        elseif number == 2 then 
+            CurrentState = hillUp
+        elseif number == 3 then
+            CurrentState = hillDown
+        else 
+            CurrentState = '_'
+        end 
+    end
+    
+    if lastState == '\\' then
+        local number = math.random(1,3)
+        if number == 1 then 
+            CurrentState = hillDown
+        elseif number == 2 then 
+            CurrentState = hillDown
+        elseif number == 3 then
+            CurrentState = '_'
+        end
+    end
 end
 
 local function temp()
-    
+        
     CurrentState = '_'
-    
-    printState()
+        
+    AddToTerrain()
 
 end
 
 local function states()
     if lastState == '' then 
-    temp() elseif
-    lastState ~= '' then
-    RandomState()
-end end
-
-states()
+        temp() 
+    elseif lastState ~= '' then
+        GenerateNextPeiceOfTerrain()
+    end 
 end
+
+print('How much terrain do you want to generate?')
+
+local terrainAmount = io.read()
+
+
+for i = 1, tonumber(terrainAmount) do
+
+    GenerateNextPeiceOfTerrain()
+
+    AddToTerrain()
+
+    states() 
+
+end
+
 print(terrain)
